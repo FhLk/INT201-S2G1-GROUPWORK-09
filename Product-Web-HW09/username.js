c4a3a53172d544c615f5846fbc14c985c97960a7
buratsakorn-bew <buratsakorn.boon@mail.kmutt.ac.th>
2021-11-24 21:04:46 +0700

    edit code

--- Product-Web-HW09/username.js
+++ Product-Web-HW09/username.js
@@ -1,12 +1,11 @@
 let username = document.getElementById('username');
 let button = document.getElementById('change');
-let color = document.getElementById('color');
 currentUser();
 
 function setName() {
     let name = prompt('Enter your name');
-    if (name == null) {
-        localStorage.setItem('username', 'Guest');
+    if (name == null || name =='') {
+        localStorage.removeItem('username');
         button.innerHTML = 'Login';
     } else {
         localStorage.setItem('username', name);
@@ -18,7 +17,7 @@ function setName() {
 button.addEventListener('click', () => {
     if ((button.innerHTML == 'Logout')) {
         button.innerHTML = 'Login';
-        localStorage.setItem('username', 'Guest');
+        localStorage.removeItem('username');
         username.innerHTML = 'Guest'
     } else {
         button.innerHTML == 'Logout';
@@ -29,11 +28,9 @@ button.addEventListener('click', () => {
 function currentUser() {
     if (localStorage.getItem('username') == null || localStorage.getItem('username') == 'null') {
         username.innerHTML = 'Guest'
+        button.innerHTML = 'Login';
     } else {
         username.innerHTML = localStorage.getItem('username');
-    }
-    if (username.innerHTML == 'Guest') {
-        button.innerHTML = 'Login';
-    } else
         button.innerHTML = 'Logout';
+    }
 }
